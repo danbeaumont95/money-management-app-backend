781f85c656035878fb22117b4d80692eb605645e
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-07-08 21:37:31 +0100

    setup before deploying

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+__name__ = '.'.join(__name__.split('/'))
+__package__ = '.'.join('.'.join(__name__.split('/')).split('.')[:-1])
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.testclient import TestClient
 from .routers import user
+# from routers import user
+
 app = FastAPI()
 origins = ["*"]
 app.include_router(user.router)
@@ -13,6 +18,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# if __name__ == "__main__":
+#     uvicorn.run("app.api:app", host="0.0.0.0", port=8080, reload=True)
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8005,
+                log_level="info", reload=True)
 # uvicorn app.main:app --reload
 # source ./django_env/bin/activate
 # go to app file and run pytest test.py -o log_cli=true -s
