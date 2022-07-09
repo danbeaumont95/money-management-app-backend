f869c85892c37ce58fc4da971c08c30583df760e
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-07-09 12:09:39 +0100

    changed main in project root

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+from .app.routers import user
+from fastapi.testclient import TestClient
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+import uvicorn
+print('hello')
+# from routers import user
 
 app = FastAPI()
+origins = ["*"]
+app.include_router(user.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
 @app.get("/")
-def home():
-    return {"message": "Hello TutLinks.com"}
+async def root():
+    return {"message": "money-management api V0.0.1!"}
